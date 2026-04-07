d695c5104d01d53d309c1e1d2d080d11eaab8552
WolverineAryan <36thoraryan@gmail.com>
2026-04-07 18:53:57 +0530

    resume and update

--- pranav-portfolio/three/HeroCanvas.tsx
+++ pranav-portfolio/three/HeroCanvas.tsx
@@ -1,12 +1,28 @@
 "use client";
 
-import dynamic from "next/dynamic";
+import { Canvas } from "@react-three/fiber";
+import { Float, OrbitControls } from "@react-three/drei";
 
-// ✅ Prevent SSR issues
-const Scene = dynamic(() => import("./HeroScene"), {
-  ssr: false,
-});
+function Sphere() {
+  return (
+    <Float speed={2} rotationIntensity={1} floatIntensity={2}>
+      <mesh>
+        <icosahedronGeometry args={[2, 1]} />
+        <meshStandardMaterial color="#00C853" wireframe />
+      </mesh>
+    </Float>
+  );
+}
 
 export default function HeroCanvas() {
-  return <Scene />;
+  return (
+    <Canvas camera={{ position: [0, 0, 6] }}>
+      <ambientLight intensity={0.5} />
+      <directionalLight position={[2, 2, 2]} />
+
+      <Sphere />
+
+      <OrbitControls enableZoom={false} enablePan={false} />
+    </Canvas>
+  );
 }
